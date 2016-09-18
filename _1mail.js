4f025f6dd0e7fda06d57792a6c7f1122dfbc1487
tben1 <tarikbenabbou@gmail.com>
2016-09-19 00:44:42 +0100

    Final commit

--- _1mail.js
+++ _1mail.js
@@ -1,4 +1,18 @@
 
+var _messages;
+var _rules;
+var _res = new Object();
+
+function filter(m,r) {
+
+	_messages = m;
+
+	_rules = rxArray(r);
+
+	Object.keys(m).forEach(messageForFunc);
+
+	return _res;
+}
 
 function rxArray(r) {
 
@@ -31,4 +45,23 @@ function RuleRegEx(str,regExp) {
 	return str.search(regExp) == 0;
 }
 
+function messageForFunc(k) {
 
+	var messageObj = _messages[k];
+
+	var from = messageObj.from;
+
+	var to = messageObj.to;
+	
+	var rArray = new Array();
+
+	for (var i=0; i < _rules.length; i++) {
+
+		MessageRule(_rules[i],messageObj,rArray);
+	}
+
+	_res[k] = rArray;
+
+}
+
+exports.filter = filter;
