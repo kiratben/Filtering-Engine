06afb1447cc21f4767f8cfdeb92298c92f487069
tben1 <tarikbenabbou@gmail.com>
2016-09-20 00:50:57 +0100

    fFunc

--- _2mail.js
+++ _2mail.js
@@ -3,6 +3,35 @@ exports.filter = function (messages, rules) {
     function fFunc(s, r) {
 
         if (r == undefined || r == '*' || s == r) return true;
+
+         var j;
+
+        for (var i = 0; i < r.length; i++) {
+
+            if (r[i] == "*") {
+
+                j = i + 1;
+
+                var mx = r.length > s.length ? r.length : s.length;
+
+                for (var k = i; k < mx; k++) {
+
+                    if (fFunc(s.substr(k, r.length - 1), r.substr(j, s.length - 1))) {
+
+                        return true;
+                    }
+                }
+                return false;
+            }
+
+            if (r[i] == "?") {
+                continue;
+            }
+            if (r[i] != s[i]) {
+                return false;
+            }
+        }
+        return true;
    
     }
 
